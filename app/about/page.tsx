8dd24196df606ef0cf0e7903124d122c0922c1b9
VieriElCrack1 <santosvieri9@gmail.com>
2025-07-28 12:00:29 -0500

    feat: Alunas muestras y pruebas de metadata y metatag, <meta> del lado del servidor, para SEO.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Description',
+ keywords: ['About Page', 'Vieri Santos', 'Information', '...']
+};
+
 export default function AboutPage() {
     return(
         <>
